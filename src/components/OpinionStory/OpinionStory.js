d0ff9d470570d199437da18ab18a77899e143ec5
Ramon Saviato <ramonsaviato@hotmail.com>
2023-09-26 11:17:31 -0300

    Update OpinionStory.js

--- src/components/OpinionStory/OpinionStory.js
+++ src/components/OpinionStory/OpinionStory.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
+import { QUERIES } from '../../constants';
+
 const OpinionStory = ({ id, title, author, avatar }) => {
   return (
     <a href={`/story/${id}`}>
@@ -15,16 +17,34 @@ const OpinionStory = ({ id, title, author, avatar }) => {
   );
 };
 
-const Wrapper = styled.article`
-  color: var(--color-gray-900);
-`;
-
 const Avatar = styled.img`
   display: block;
   width: 48px;
   height: 48px;
   border-radius: 50%;
   object-fit: cover;
+  margin-left: 32px;
+
+  @media ${QUERIES.tabletOnly} {
+    margin-left: revert;
+    margin-bottom: 8px;
+  }
+  
+  @media ${QUERIES.laptopAndUp} {
+    margin-left: 16px;
+  }
+`;
+
+const Wrapper = styled.article`
+  color: var(--color-gray-900);
+
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-between;
+
+  @media ${QUERIES.tabletOnly} {
+    display: revert;
+  }
 `;
 
 const AuthorName = styled.p`
